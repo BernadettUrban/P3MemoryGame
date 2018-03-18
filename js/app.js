eb92b90dab779aeb09961192c5396b8db1c5776a
Bernadett Urban <bernadett.urban@hotmail.com>
2018-03-18 10:16:24 +0100

    feat: display the card's symbol and add the card to a *list* of open cards and check to see if the two cards match

--- js/app.js
+++ js/app.js
@@ -33,6 +33,7 @@ function startGame (){
         [].forEach.call(shuffledCards, function(item) {
             deck.appendChild(item);
         });
+        cards[i].classList.remove("show", "open", "match");
     }
 }
 
@@ -48,3 +49,66 @@ document.body.onload = startGame();
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+
+ //display the card's symbol
+ function displaySymbol (){
+    this.classList.toggle("open");
+    this.classList.toggle("show");
+    this.classList.toggle("disabled");
+ };
+
+ var openedCards = [];
+ function openCards(){
+    openedCards.push(this);
+    var len = openedCards.length;
+    if(len === 2){
+        if (openedCards[0].type === openedCards[1].type){
+            matched();
+        } else {
+            unmatched();
+        }
+    }  
+ };
+
+ let matchedCard = document.getElementsByClassName("match");
+ function matched(){
+    openedCards[0].classList.add("match");
+    openedCards[1].classList.add("match");
+    openedCards[0].classList.remove("show", "open");
+    openedCards[1].classList.remove("show", "open");
+    openedCards = [];
+};
+
+function unmatched(){
+    openedCards[0].classList.add("unmatched");
+    openedCards[1].classList.add("unmatched");
+    disable();
+    setTimeout(function(){
+        openedCards[0].classList.remove("show", "open", "unmatched");
+        openedCards[1].classList.remove("show", "open", "unmatched");
+        enable();
+        openedCards = [];
+    },1100);
+}
+
+function disable(){
+    Array.prototype.filter.call(cards, function(card){
+        card.classList.add('disabled');
+    });
+}
+function enable(){
+    Array.prototype.filter.call(cards, function(card){
+        card.classList.remove('disabled');
+        for(var i = 0; i < matchedCard.length; i++){
+            matchedCard[i].classList.add("disabled");
+        }
+    });
+}
+
+ // add event listeners to each card
+ for (var i = 0; i < cards.length; i++){
+    cards[i].addEventListener("click", displaySymbol);
+    cards[i].addEventListener("click", openCards);
+ };
